70767e06653d95f9f4c8538ebb97191ff03a544a
Isaac Xu <isaac.xu@dal.ca>
2021-08-13 16:38:02 -0300

    BUG: Fixed bug for target encoder fc and encoder fc interference with deepcopy

--- byol/builder.py
+++ byol/builder.py
@@ -4,9 +4,11 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+import copy
+
 import torch
 import torch.nn as nn
-from utilities import ema, set_requires_grad
+import utilities
 
 
 class BYOL(nn.Module):
@@ -26,12 +28,11 @@ class BYOL(nn.Module):
     def __init__(self, base_encoder, init_from_online, dim=2048, pred_dim=512):
         super(BYOL, self).__init__()
 
-        # create the encoder
+        # create the online encoder
         # num_classes is the output fc dimension, zero-initialize last BNs
         self.encoder = base_encoder(num_classes=dim, zero_init_residual=True)
-        self.target_encoder = base_encoder(num_classes=dim, zero_init_residual=True)
 
-        # build a 3-layer projector
+        # build a 3-layer online projector
         prev_dim = self.encoder.fc.weight.shape[1]
         self.encoder.fc = nn.Sequential(
             nn.Linear(prev_dim, prev_dim, bias=False),
@@ -47,19 +48,11 @@ class BYOL(nn.Module):
             6
         ].bias.requires_grad = False  # hack: not use bias as it is followed by BN
 
-        # build same projector for target model
-        self.target_encoder.fc = nn.Sequential(
-            nn.Linear(prev_dim, prev_dim, bias=False),
-            nn.BatchNorm1d(prev_dim),
-            nn.ReLU(inplace=True),  # first layer
-            nn.Linear(prev_dim, prev_dim, bias=False),
-            nn.BatchNorm1d(prev_dim),
-            nn.ReLU(inplace=True),  # second layer
-            self.encoder.fc,
-            nn.BatchNorm1d(dim, affine=False),
-        )  # output layer
+        # copy target model from online model
+        self.target_encoder = copy.deepcopy(self.encoder)
 
-        set_requires_grad(self.target_encoder, False)
+        # disable grad calculations for target model
+        utilities.set_requires_grad(self.target_encoder, False)
 
         if init_from_online:
             self.update_target(self.target_encoder, self.encoder, None)
@@ -82,15 +75,13 @@ class BYOL(nn.Module):
             First view of images.
         x2 : torch.Tensor
             Second view of images.
-
         Return
         ------
         p1, p2, z1, z2 :
             online predictors and target projections of the networks
-
         Note
         ----
-        See Sec. 3 of https://arxiv.org/abs/2006.07733 for detailed notations
+        See https://arxiv.org/abs/2006.07733 for detailed notations
         """
         # compute features for one view
         z1 = self.encoder(x1)  # NxC
@@ -106,7 +97,7 @@ class BYOL(nn.Module):
     def update_target(self, target_model, online_model, alpha=0.99):
         target_state_dict = target_model.state_dict()
         for param in target_state_dict:
-            target_state_dict[param] = ema(
+            target_state_dict[param] = utilities.ema(
                 target_state_dict[param], online_model.state_dict()[param], alpha
             )
         target_model.load_state_dict(target_state_dict)
