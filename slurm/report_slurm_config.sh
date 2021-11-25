d227beb6484bdf4d480fdcd5e34c0ae49a7e38fe
Scott Lowe <scott.code.lowe@gmail.com>
2021-11-25 17:29:54 -0400

    JOB: Remove debug info for unavailable SLURM env variables

--- slurm/report_slurm_config.sh
+++ slurm/report_slurm_config.sh
@@ -26,26 +26,10 @@ echo "SLURM_TASKS_PER_NODE    = $SLURM_TASKS_PER_NODE"      # Number of tasks to
 echo "SLURM_NTASKS            = $SLURM_NTASKS"              # Number of tasks to spawn.
 echo "SLURM_PROCID            = $SLURM_PROCID"              # The MPI rank (or relative process ID) of the current process
 echo ""
-echo "SBATCH_GRES             = $SBATCH_GRES"               # Same as --gres
-echo "SLURM_GRES              = $SLURM_GRES"                #
-echo "SBATCH_GPU_BIND         = $SBATCH_GPU_BIND"           # Same as --gpu-bind
-echo "SLURM_GPU_BIND          = $SLURM_GPU_BIND"            # Requested binding of tasks to GPU. Only set if the --gpu-bind option is specified.
-echo "SBATCH_GPU_FREQ         = $SBATCH_GPU_FREQ"           # Number of GPUs allocated to the batch step.
-echo "SLURM_GPU_FREQ          = $SLURM_GPU_FREQ"            # Requested GPU frequency. Only set if the --gpu-freq option is specified.
-echo "SBATCH_GPUS             = $SBATCH_GPUS"               # Same as -G, --gpus
-echo "SLURM_GPUS              = $SLURM_GPUS"                # Number of GPUs requested. Only set if the -G, --gpus option is specified.
-echo "SBATCH_GRES_FLAGS       = $SBATCH_GRES_FLAGS"         # Same as --gres-flags
-echo "SLURM_GPUS_ON_NODE      = $SLURM_GPUS_ON_NODE"        # Number of GPUs allocated to the batch step.
-echo "SLURM_GPUS_PER_NODE     = $SLURM_GPUS_PER_NODE"       # Requested GPU count per allocated node. Only set if the --gpus-per-node option is specified
-echo "SLURM_GPUS_PER_SOCKET   = $SLURM_GPUS_PER_SOCKET"     # Requested GPU count per allocated socket. Only set if the --gpus-per-socket option is specified.
-echo "SLURM_GPUS_PER_TASK     = $SLURM_GPUS_PER_TASK"       # Requested GPU count per allocated task. Only set if the --gpus-per-task option is specified.
-echo ""
 echo "GPUS_PER_NODE           = $GPUS_PER_NODE"             # Manually set number of GPUs.
 echo "SLURM_CPUS_ON_NODE      = $SLURM_CPUS_ON_NODE"        # Number of CPUs allocated to the batch step.
 echo "SLURM_JOB_CPUS_PER_NODE = $SLURM_JOB_CPUS_PER_NODE"   # Count of CPUs available to the job on the nodes in the allocation.
 echo "SLURM_CPUS_PER_TASK     = $SLURM_CPUS_PER_TASK"       # Number of cpus requested per task. Only set if the --cpus-per-task option is specified.
-echo "SLURM_MEM_PER_CPU       = $SLURM_MEM_PER_CPU"         # Same as --mem-per-cpu
-echo "SLURM_MEM_PER_GPU       = $SLURM_MEM_PER_GPU"         # Same as --mem-per-gpu
 echo "SLURM_MEM_PER_NODE      = $SLURM_MEM_PER_NODE"        # Same as --mem
 echo ""
 echo "------------------------------------"
